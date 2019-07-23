a752faf9f2ffa62f84dc47d68113e4c2e9d274da
l2479503860 <2479503860@qq.com>
2019-07-23 11:10:32 +0800

    没有yml 配置文件等请自己添加

--- qxy/qxy-web/src/main/java/com/qxy/SpringBootRun.java
+++ qxy/qxy-web/src/main/java/com/qxy/SpringBootRun.java
@@ -1,11 +1,11 @@
 package com.qxy;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@MapperScan("com.jt.mapper")
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
 public class SpringBootRun {
 	
 	public static void main(String[] args) {
